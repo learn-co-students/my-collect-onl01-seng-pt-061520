505b8ec1ca4d251f86eb693869b28f43239003c7
IgoVeyner <igorveyner95@gmail.com>
2020-06-17 19:55:38 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+    i = 0
+    collection = []
+    while i < array.length
+        if array[0].start_with?("T")
+            collection <<  array[i].split(" ").first
+        else
+            collection << array[i].upcase
+        end
+        i += 1
+    end
+    collection
+end
